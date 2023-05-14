68814faa4470147849c989a5e270564a132cd292
james8548 <119172802+james8548@users.noreply.github.com>
2023-05-14 02:24:45 +0200

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,16 +7,6 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.svg";
-import python from "./tech/python.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -36,16 +26,7 @@ export {
   github,
   menu,
   close,
-  css,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  tailwind,
-  threejs,
-  python,
+  
   meta,
   shopify,
   starbucks,
